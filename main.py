a717a7a3471535f7c6ea457b23ef6c8f4a453970
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-11-30 14:00:19 +0530

    Fixed buttons

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ genre = st.radio(
     [":rainbow[nmap]", "***Steganography***", "Hashcat"],
     captions = ["Nmap your ass.", "Stegonograph you.", "Get your hash."])
 
-if st.button("Submit"):
-    if genre == ":rainbow[nmap]":
+button1 = st.button('Lets Go!')
+if st.session_state.get('button') != True:
+
+    st.session_state['button'] = button1
+
+if (st.session_state['button'] == True):
+    if genre==":rainbow[nmap]":
         options = ["-sS"]
         host = st.text_input('Host', '127.0.0.1')
         command = ['nmap'] + options + [host]
-        result = subprocess.run(command, capture_output=True, text=True)
+        if st.button('Run!'):
+            result = subprocess.run(command, capture_output=True, text=True)
